879dc26a1196b1bce7cc308671f3edc2c04a7245
arulmary19 <arulmary.roche@gmail.com>
2025-11-16 11:05:34 +0300

    commit

---
+++ BIMS/Migrations/20251116073711_AddInsurancePlansTable.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BIMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInsurancePlansTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InsurancePlans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InsuranceClientId = table.Column<int>(type: "int", nullable: false),
+                    PlanName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PlanNameAr = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    DescriptionAr = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InsurancePlans", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InsurancePlans_InsuranceClients_InsuranceClientId",
+                        column: x => x.InsuranceClientId,
+                        principalTable: "InsuranceClients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "CustomerTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 16, 7, 37, 9, 205, DateTimeKind.Utc).AddTicks(5169));
+
+            migrationBuilder.UpdateData(
+                table: "CustomerTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 16, 7, 37, 9, 205, DateTimeKind.Utc).AddTicks(5172));
+
+            migrationBuilder.UpdateData(
+                table: "CustomerTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 16, 7, 37, 9, 205, DateTimeKind.Utc).AddTicks(5173));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InsurancePlans_InsuranceClientId",
+                table: "InsurancePlans",
+                column: "InsuranceClientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InsurancePlans");
+
+            migrationBuilder.UpdateData(
+                table: "CustomerTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 14, 18, 33, 23, 200, DateTimeKind.Utc).AddTicks(1996));
+
+            migrationBuilder.UpdateData(
+                table: "CustomerTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 14, 18, 33, 23, 200, DateTimeKind.Utc).AddTicks(1997));
+
+            migrationBuilder.UpdateData(
+                table: "CustomerTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 11, 14, 18, 33, 23, 200, DateTimeKind.Utc).AddTicks(1999));
+        }
+    }
+}
